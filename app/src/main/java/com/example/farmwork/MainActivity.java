ee504e897a05858fcfb19622b4fc0f60edf81e22
Aravind Jagarlamudi <o161800@rguktrkv.ac.in>
2021-01-09 19:54:35 +0530

    Languages

--- app/src/main/java/com/example/farmwork/MainActivity.java
+++ app/src/main/java/com/example/farmwork/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.farmwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Bundle bundle = getIntent().getExtras();
-        text = bundle.getString("message");
+        text = bundle.getString("mytext");
+        if (text == "हिंदी") {
+            setLocale("hi");
+            recreate();
+        }
+    }
+    private void setLocale(String lang){
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
     }
 }
